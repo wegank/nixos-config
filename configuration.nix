5f31f18b2ea7363fac6a6fb07f6a0ac0b8d5dc24
Weijia Wang <9713184+wegank@users.noreply.github.com>
2020-12-19 21:11:00 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -10,36 +10,44 @@
       ./hardware-configuration.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  
-  # Use the latest Linux kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot = {
+    # Use the latest Linux kernel.
+    kernelPackages = pkgs.linuxPackages_latest;
+    loader = {
+      efi.canTouchEfiVariables = true;
+      # Use the systemd-boot EFI boot loader.
+      systemd-boot.enable = true;
+    };
+  };
 
-  # Define your hostname.
-  networking.hostName = "workstation";
-  # Disable wireless support via wpa_supplicant.
-  networking.wireless.enable = false;
-  # networking.networkmanager.unmanaged = [
-  #   "*" "except:type:wwan" "except:type:gsm"
-  # ];
-
-  # Set your time zone.
+  # Set time zone.
   time.timeZone = "Europe/Paris";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp0s5.useDHCP = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking = {
+    # Set hostname.
+    hostName = "workstation";
+    # Disable wireless support.
+    wireless.enable = false;
+    # Enable DHCP.
+    useDHCP = false;
+    interfaces.enp0s5.useDHCP = true;
+    # Open ports in the firewall.
+    # firewall.allowedTCPPorts = [ ... ];
+    # firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # firewall.enable = false;
+  };
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "fr_FR.UTF-8";
+  i18n = {
+    defaultLocale = "fr_FR.UTF-8";
+    inputMethod = {
+      # Enable Fcitx.
+      enabled = "fcitx";
+      fcitx.engines = with pkgs.fcitx-engines; [ libpinyin cloudpinyin ];
+    };
+  };
+
   console = {
     font = "Lat2-Terminus16";
     keyMap = "us";
@@ -50,41 +58,41 @@
     noto-fonts-cjk
   ];
 
-  # Enable Fcitx.
-  i18n.inputMethod = {
-    enabled = "fcitx";
-    fcitx.engines = with pkgs.fcitx-engines; [ libpinyin cloudpinyin ];
-  };
-  
-  # Enable the Plasma 5 desktop.
-  services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-  
-  # Enable autologin.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "weijia";
-  
-  # Configure keymap in X11.
-  services.xserver.layout = "us";
-  services.xserver.xkbOptions = "eurosign:e";
+  services = {
+    xserver = {
+      enable = true;
+      # Configure keymap in X11.
+      layout = "us";
+      xkbOptions = "eurosign:e";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+      displayManager = {
+        # Enable SDDM.
+        sddm.enable = true;
+        # Enable autologin.
+        autoLogin.enable = true;
+        autoLogin.user = "weijia";
+      };
+      # Enable the Plasme 5 desktop.
+      desktopManager.plasma5.enable = true;
+      # Enable touchpad support.
+      libinput.enable = true;
+    };
+    # Enable CUPS to print documents.
+    printing.enable = true;
+  };
 
   # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-
   # Define a user account.
-  users.mutableUsers = false;
-  users.users.weijia = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ];
-    password = import ./password.nix;
+  users = {
+    mutableUsers = false;
+    users.weijia = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" "networkmanager" ];
+      password = import ./password.nix;
+    };
   };
 
   # List packages installed in system profile. To search, run:
@@ -94,31 +102,20 @@
     glxinfo firefox
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
+  programs = {
+    # Enable SUID wrappers.
+    mtr.enable = true;
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+    # Enable Oh my ZSH.
+    zsh.ohMyZsh = {
+      enable = true;
+      plugins = [ "git" "python" "man" ];
+      theme = "agnoster";
+    };
   };
-
-  # Enable Oh my ZSH.
-  programs.zsh.ohMyZsh = {
-    enable = true;
-    plugins = [ "git" "python" "man" ];
-    theme = "agnoster";
-  };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -127,5 +124,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "20.09"; # Did you read the comment?
-
 }
