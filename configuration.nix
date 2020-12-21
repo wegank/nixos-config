bd6bafbf1fc3f46176553bc9f5c04d3a89728ff2
Weijia Wang <9713184+wegank@users.noreply.github.com>
2020-12-21 10:12:53 +0100

    Update config

--- configuration.nix
+++ configuration.nix
@@ -31,11 +31,6 @@
     # Enable DHCP.
     useDHCP = false;
     interfaces.enp0s5.useDHCP = true;
-    # Open ports in the firewall.
-    # firewall.allowedTCPPorts = [ ... ];
-    # firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # firewall.enable = false;
   };
 
   # Select internationalisation properties.
@@ -129,19 +124,10 @@
     };
   };
 
-  nixpkgs.config = {
-    allowUnfree = true;
-  };
-
   environment.systemPackages = with pkgs; [
     firefox
   ];
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.09"; # Did you read the comment?
+  # NixOS release.
+  system.stateVersion = "20.09";
 }
