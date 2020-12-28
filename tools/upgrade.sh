8e36cacf6fee5876e5b9da2069e9a5cd3bf16824
Weijia Wang <9713184+wegank@users.noreply.github.com>
2020-12-28 02:27:48 +0100

    Update script

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -1,4 +1,7 @@
 #!/run/current-system/sw/bin/bash
-cp configuration.nix /etc/nixos/
-cp home.nix /etc/nixos/
-nixos-rebuild switch --upgrade
+git clone . /tmp/nixos-config
+nixos-generate-config --dir /tmp/nixos-config
+git -C /tmp/nixos-config add .
+git -C /tmp/nixos-config -c user.name='x' -c user.email='x' commit -m "Upgrade"
+nixos-rebuild switch --upgrade --flake '/tmp/nixos-config/.' --no-write-lock-file
+rm -rf /tmp/nixos-config
