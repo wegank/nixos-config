61c8bae8a4e79589887e7fbfb2a3a9fd06b6ebae
Weijia Wang <9713184+wegank@users.noreply.github.com>
2020-12-30 05:49:18 +0100

    Build prl_vid

--- hardware/parallels-unfree/prl-tools.nix
+++ hardware/parallels-unfree/prl-tools.nix
@@ -79,6 +79,7 @@ stdenv.mkDerivation rec {
         cp prl_tg/Toolgate/Guest/Linux/prl_tg/prl_tg.ko $out/lib/modules/${kernelVersion}/extra
         cp prl_fs/SharedFolders/Guest/Linux/prl_fs/prl_fs.ko $out/lib/modules/${kernelVersion}/extra
         cp prl_fs_freeze/Snapshot/Guest/Linux/prl_freeze/prl_fs_freeze.ko $out/lib/modules/${kernelVersion}/extra
+        cp prl_vid/Video/Guest/Linux/kmod/prl_vid.ko $out/lib/modules/${kernelVersion}/extra
       )
     fi
 
