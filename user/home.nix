31125e5797b10c700443498040d084e6d6f0c50b
Weijia Wang <9713184+wegank@users.noreply.github.com>
2021-11-11 04:47:22 +0100

    Modify resolution

--- user/home.nix
+++ user/home.nix
@@ -20,8 +20,8 @@
       modifier = "Mod4";
       output = {
         "Virtual-1" = {
-          mode = "1920x1080";
-          scale = "1.1";
+          mode = "1680x1050";
+          # scale = "1.1";
         };
       };
       terminal = "alacritty";
