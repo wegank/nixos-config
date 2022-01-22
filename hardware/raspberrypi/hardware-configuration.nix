7d6821074e451ee4cceac159bdb12a227d2cf00a
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-01-22 20:11:59 +0100

    Deploy firmware

--- hardware/raspberrypi/hardware-configuration.nix
+++ hardware/raspberrypi/hardware-configuration.nix
@@ -36,4 +36,27 @@
   };
 
   swapDevices = [ ];
+
+  system.activationScripts.firmware-update =
+    let configTxt = pkgs.writeText "config.txt" ''
+      [pi4]
+      kernel=u-boot-rpi4.bin
+      enable_gic=1
+      armstub=armstub8-gic.bin
+      disable_overscan=1
+      [all]
+      arm_64bit=1
+      enable_uart=1
+      avoid_warnings=1
+    '';
+    in
+    ''
+      (cd ${pkgs.raspberrypifw}/share/raspberrypi/boot && cp bootcode.bin fixup*.dat start*.elf /boot/)
+      # Add the config
+      cp ${configTxt} /boot/config.txt
+      # Add pi4 specific files
+      cp ${pkgs.ubootRaspberryPi4_64bit}/u-boot.bin /boot/u-boot-rpi4.bin
+      cp ${pkgs.raspberrypi-armstubs}/armstub8-gic.bin /boot/armstub8-gic.bin
+      cp ${pkgs.raspberrypifw}/share/raspberrypi/boot/bcm2711-rpi-4-b.dtb /boot/
+    '';
 }
