3552193351ccc35ea8d780bbee7db1eee7fed9d7
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-01-22 22:58:28 +0100

    Test XRDP

--- system/server.nix
+++ system/server.nix
@@ -9,9 +9,14 @@
       };
       displayManager.defaultSession = "xfce";
     };
-
     sshd.enable = true;
+    xrdp = {
+      enable = true;
+      defaultWindowManager = "xfce4-session";
+    };
   };
+
+  networking.firewall.allowedTCPPorts = [ 3389 ];
 
   boot.binfmt.emulatedSystems = [
     "riscv64-linux"
