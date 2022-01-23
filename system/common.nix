1cbd784ca383b56bd5be0b5033e2616af0d038fd
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-01-23 11:17:53 +0100

    Enable nix-gc

--- system/common.nix
+++ system/common.nix
@@ -5,8 +5,16 @@
 {
   nix = {
     package = pkgs.nixFlakes;
+    autoOptimiseStore = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
     extraOptions = ''
       experimental-features = nix-command flakes
+      min-free = ${toString (100 * 1024 * 1024)}
+      max-free = ${toString (1024 * 1024 * 1024)}
     '';
   };
 
