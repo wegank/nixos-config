faf0dfea53784202c4391150c80a8ffe09112849
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-01-23 14:11:44 +0100

    Add owner info

--- flake.nix
+++ flake.nix
@@ -6,54 +6,61 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, home-manager, nixpkgs }: {
-    nixosConfigurations = builtins.mapAttrs
-      (hostname: _:
-        let configs = {
-          parallels = {
-            platform = "aarch64-linux";
-            profile = "desktop";
-          };
-          parallels-unfree = {
-            platform = "aarch64-linux";
-            profile = "desktop";
-          };
-          raspberrypi = {
-            platform = "aarch64-linux";
-            profile = "server";
-          };
-          vmware = {
-            platform = "x86_64-linux";
-            profile = "desktop";
-          };
-        }; in
-        nixpkgs.lib.nixosSystem {
-          system = configs.${hostname}.platform;
-          modules = [
-            # Hardware configuration.
-            (./hardware + "/${hostname}" + /hardware-configuration.nix)
-            # System configuration.
-            ./system/base.nix
-            (./system + "/${configs.${hostname}.profile}.nix")
-            # Home Manager.
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useUserPackages = true;
-                useGlobalPkgs = true;
-                extraSpecialArgs = {
-                  profile = configs.${hostname}.profile;
+  outputs = { self, home-manager, nixpkgs }:
+    let
+      owner = {
+        name = "weijia";
+        description = "Weijia Wang";
+      };
+      machines = {
+        parallels = {
+          platform = "aarch64-linux";
+          profile = "desktop";
+        };
+        parallels-unfree = {
+          platform = "aarch64-linux";
+          profile = "desktop";
+        };
+        raspberrypi = {
+          platform = "aarch64-linux";
+          profile = "server";
+        };
+        vmware = {
+          platform = "x86_64-linux";
+          profile = "desktop";
+        };
+      };
+    in
+    {
+      nixosConfigurations = builtins.mapAttrs
+        (hostname: _:
+          nixpkgs.lib.nixosSystem {
+            system = machines.${hostname}.platform;
+            modules = [
+              # Hardware configuration.
+              (./hardware + "/${hostname}" + /hardware-configuration.nix)
+              # System configuration.
+              ./system/base.nix
+              (./system + "/${machines.${hostname}.profile}.nix")
+              # Home Manager.
+              home-manager.nixosModules.home-manager
+              {
+                home-manager = {
+                  useUserPackages = true;
+                  useGlobalPkgs = true;
+                  extraSpecialArgs = {
+                    profile = machines.${hostname}.profile;
+                  };
+                  users = builtins.mapAttrs
+                    (username: _:
+                      ./users + "/${username}" + /home.nix
+                    )
+                    (builtins.readDir ./users);
                 };
-                users = builtins.mapAttrs
-                  (username: _:
-                    ./users + "/${username}" + /home.nix
-                  )
-                  (builtins.readDir ./users);
-              };
-            }
-          ];
-        }
-      )
-      (builtins.readDir ./hardware);
-  };
+              }
+            ];
+          }
+        )
+        (builtins.readDir ./hardware);
+    };
 }
