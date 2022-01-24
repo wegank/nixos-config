82a278e6a2cea21cce5ba393badf6e0ae535aaf0
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-01-24 13:52:37 +0100

    Update import

--- system/server.nix
+++ system/server.nix
@@ -2,7 +2,10 @@
 
 {
   imports = [
-    (fetchTarball "https://github.com/msteen/nixos-vscode-server/tarball/master")
+    (fetchTarball {
+      url = https://github.com/msteen/nixos-vscode-server/tarball/bc28cc2a7d866b32a8358c6ad61bea68a618a3f5;
+      sha256 = "00aqwrr6bgvkz9bminval7waxjamb792c0bz894ap8ciqawkdgxp";
+    })
   ];
 
   services = {
