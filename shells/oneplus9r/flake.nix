ba828304752025eb7e9fae321f58a05a3b682fbe
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-01-28 20:17:08 +0100

    Add source

--- shells/oneplus9r/flake.nix
+++ shells/oneplus9r/flake.nix
@@ -13,12 +13,20 @@
         owner = "YumeMichi";
         repo = "device_oneplus_oneplus9r";
         rev = "e5488d45daa0f7b2c4f2abb720b26af548824914";
+        sha256 = "sha256-ULi+9XF6HMuHHRmQwZ0liJhLzLe9VM2bWXzTBn7nwNw=";
       };
 
       source.dirs."kernel/oneplus/sm8250".src = pkgs.fetchFromGitHub {
         owner = "YumeMichi";
         repo = "kernel_oneplus_sm8250";
         rev = "e738058736bc87219414767c443b9b77dc611990";
+        sha256 = "sha256-ULi+9XF6HMuHHRmQwZ0liJhLzLe9VM2bWXzTBn7nwNw=";
+      };
+
+      source.dirs."hardware/oneplus".src = pkgs.fetchFromGitHub {
+        owner = "YumeMichi";
+        repo = "hardware_oneplus";
+        rev = "6ee77f3f666fd3698a74d99eb12b0eeab575da71";
       };
 
       source.dirs."vendor/oneplus".src = pkgs.fetchFromGitHub {
@@ -29,7 +37,7 @@
 
       ccache.enable = true;
     });
-    
+
     defaultPackage.aarch64-linux = self.robotnixConfigurations."oneplus9r".img;
   };
 }
