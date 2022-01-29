a2cc15a535c383c0d7b3a4df9426b1dd9e7a352c
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-01-29 11:45:42 +0100

    Update hashes

--- shells/oneplus9r/flake.nix
+++ shells/oneplus9r/flake.nix
@@ -4,26 +4,26 @@
   inputs.robotnix.url = "github:danielfullmer/robotnix";
 
   outputs = { self, robotnix }: {
-    robotnixConfigurations."oneplus9r" = robotnix.lib.robotnixSystem ({ config, pkgs, ... }: {
-      device = "oneplus9r";
+    robotnixConfigurations."lemonades" = robotnix.lib.robotnixSystem ({ config, pkgs, ... }: {
+      device = "lemonades";
       flavor = "lineageos";
       androidVersion = 11;
 
       apps.chromium.enable = false;
       webview.chromium.enable = false;
 
-      source.dirs."device/oneplus/oneplus9r".src = pkgs.fetchFromGitHub {
+      source.dirs."device/oneplus/lemonades".src = pkgs.fetchFromGitHub {
         owner = "YumeMichi";
-        repo = "device_oneplus_oneplus9r";
-        rev = "e5488d45daa0f7b2c4f2abb720b26af548824914";
-        sha256 = "sha256-LNauR62SbjvpCOBzW8tIlTDo7mnJMx6lt86Ihd24Q7o=";
+        repo = "device_oneplus_lemonades";
+        rev = "fae2e4efa49efea80af632d00762b6acd6b5cbff";
+        sha256 = "sha256-CyXIwRRCoCFYB+jDXgFbY83bx35zv3wtNJgZT4Hjqnw=";
       };
 
       source.dirs."kernel/oneplus/sm8250".src = pkgs.fetchFromGitHub {
         owner = "YumeMichi";
         repo = "kernel_oneplus_sm8250";
-        rev = "e738058736bc87219414767c443b9b77dc611990";
-        sha256 = "sha256-ULi+9XF6HMuHHRmQwZ0liJhLzLe9VM2bWXzTBn7nwNw=";
+        rev = "da96fc8381685b615f045014b7e1175494c1cd2d";
+        sha256 = "sha256-fVptlGq9Ng1Uv5EQl+m2taIPPDqkiwT9lxo3EUweqTE=";
       };
 
       source.dirs."hardware/oneplus".src = pkgs.fetchFromGitHub {
@@ -36,13 +36,13 @@
       source.dirs."vendor/oneplus".src = pkgs.fetchFromGitHub {
         owner = "YumeMichi";
         repo = "proprietary_vendor_oneplus";
-        rev = "9100879f5dbc555e45dca35040289eee44a1afcd";
-        sha256 = "sha256-fyCC/RQ7E9wYxbXQd5WtsKanh8deNOnfNqEYWLCG2JI=";
+        rev = "6c13fdcc88117365bf8ef37ebf337a4bda227a70";
+        sha256 = "sha256-CyGb0+GGhiFOsfSvM1hZxgCYpVjzevy4AMQZ2xDNFYI=";
       };
 
       ccache.enable = true;
     });
 
-    defaultPackage.aarch64-linux = self.robotnixConfigurations."oneplus9r".img;
+    defaultPackage.aarch64-linux = self.robotnixConfigurations."lemonades".img;
   };
 }
