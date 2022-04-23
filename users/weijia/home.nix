7e68db73179fe05758291202bbab3886cd5e702a
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-04-23 04:17:20 +0200

    Update .zshenv

--- users/weijia/home.nix
+++ users/weijia/home.nix
@@ -65,6 +65,8 @@ in
             "robbyrussell"
         );
       };
+      envExtra = lib.optionalString (!isLinux)
+        "export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels\${NIX_PATH:+:$NIX_PATH}\n";
     };
   };
 }
