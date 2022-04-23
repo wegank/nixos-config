c6c9a38950217e4e6be2dcbc2fe9150a605d7fd5
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-04-23 04:12:27 +0200

    Add MacBook configs

--- flake.nix
+++ flake.nix
@@ -8,15 +8,42 @@
 
   outputs = { self, home-manager, nixpkgs }:
     let
+      attrsets = nixpkgs.lib.attrsets;
+      strings = nixpkgs.lib.strings;
       metadata = builtins.fromTOML (builtins.readFile ./flake.toml);
+
+      # Filter machines by suffix.
       filterMachines = suffix:
-        (nixpkgs.lib.attrsets.filterAttrs
-          (_: config: nixpkgs.lib.strings.hasSuffix "linux" config.platform)
+        (attrsets.filterAttrs 
+          (_: config: strings.hasSuffix suffix config.platform)
           metadata.machines);
+
+      # Convert full name to username.
+      parseName = name:
+        (builtins.replaceStrings [" " "-"] ["" ""] (strings.toLower name));
     in
     {
+      # macOS configurations.
+      homeConfigurations = builtins.mapAttrs
+        (hostname: host:
+          home-manager.lib.homeManagerConfiguration {
+            extraSpecialArgs = {
+              owner = metadata.owner;
+              inherit host;
+            };
+            configuration =
+              import (./users + "/${metadata.owner.name}" + /home.nix);
+            system = host.platform;
+            homeDirectory = "/Users/${parseName metadata.owner.fullName}";
+            username = parseName metadata.owner.fullName;
+            stateVersion = "21.11";
+          }
+        )
+        (filterMachines "darwin");
+      
+      # NixOS configurations.
       nixosConfigurations = builtins.mapAttrs
-        (hostname: _:
+        (hostname: host:
           nixpkgs.lib.nixosSystem {
             system = metadata.machines.${hostname}.platform;
             specialArgs = {
@@ -27,7 +54,7 @@
               (./hardware + "/${hostname}" + /hardware-configuration.nix)
               # System configuration.
               ./system/base.nix
-              (./system + "/${metadata.machines.${hostname}.profile}.nix")
+              (./system + "/${host.profile}.nix")
               # Home Manager.
               home-manager.nixosModules.home-manager
               {
@@ -36,7 +63,7 @@
                   useGlobalPkgs = true;
                   extraSpecialArgs = {
                     owner = metadata.owner;
-                    host = metadata.machines.${hostname};
+                    inherit host;
                   };
                   users = builtins.mapAttrs
                     (username: _:
