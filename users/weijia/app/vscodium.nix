633a1560a8d9c2fe1b6e5f895f845ca81cac4fe3
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-04-23 05:51:48 +0200

    Add a bunch of extensions

--- users/weijia/app/vscodium.nix
+++ users/weijia/app/vscodium.nix
@@ -2,6 +2,7 @@
 
 let
   isLinux = lib.strings.hasSuffix "linux" host.platform;
+  package = if isLinux then pkgs.vscodium else pkgs.vscode;
 in
 {
   home = {
@@ -14,18 +15,32 @@ in
 
   programs = {
     vscode = {
-      enable = isLinux;
+      enable = true;
       package = (pkgs.vscode-with-extensions.override {
-        vscode = pkgs.vscodium;
+        vscode = package;
         vscodeExtensions = (with pkgs.vscode-extensions; [
+          # LaTeX
+          james-yu.latex-workshop
           # Markdown
           davidanson.vscode-markdownlint
           yzhang.markdown-all-in-one
           # Nix
           jnoortheen.nix-ide
+          # OCaml
+          ocamllabs.ocaml-platform
+          # Python
+          ms-python.python
+          ms-python.vscode-pylance
           # Shell
           foxundermoon.shell-format
         ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          # French Language Pack
+          {
+            publisher = "MS-CEINTL";
+            name = "vscode-language-pack-fr";
+            version = "1.67.0";
+            sha256 = "sha256-+iZAPs8eLOMQQ0akR0/hmWHw3Py/3IaTOmwKEbaCZxA=";
+          }
           # Org mode
           {
             publisher = "vscode-org-mode";
@@ -33,19 +48,32 @@ in
             version = "1.0.0";
             sha256 = "sha256-o9CIjMlYQQVRdtTlOp9BAVjqrfFIhhdvzlyhlcOv5rY=";
           }
+          # Wolfram
+          {
+            publisher = "WolframResearch";
+            name = "wolfram";
+            version = "1.5.0";
+            sha256 = "sha256-DZ73DhwkdqMeSq98Nr6Z6WGcu2js+U7O3nF6kV000sM=";
+          }
         ];
       }).overrideAttrs (old: {
-        inherit (pkgs.vscodium) pname version;
+        inherit (package) pname version;
       });
       userSettings = {
-        "terminal.integrated.fontFamily" =
-          "'Meslo LG S for Powerline'";
+        "diffEditor.ignoreTrimWhitespace" = false;
         "git.confirmSync" = false;
         "git.enableSmartCommit" = true;
-        "update.mode" = "none";
-        "diffEditor.ignoreTrimWhitespace" = false;
         "nix.enableLanguageServer" = true;
+        "terminal.integrated.fontFamily" =
+          "'Meslo LG S for Powerline'";
+        "update.mode" = "none";
+        "editor.inlineSuggest.enabled" = true;
       };
     };
   };
+
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "vscode"
+    "vscode-extension-MS-python-vscode-pylance"
+  ];
 }
