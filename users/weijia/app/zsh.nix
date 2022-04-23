60122f844690d82bd4ae25498e45c6f985f0db7d
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-04-23 06:51:17 +0200

    Update env

--- users/weijia/app/zsh.nix
+++ users/weijia/app/zsh.nix
@@ -1,9 +1,15 @@
 { pkgs, lib, host, ... }:
 
 let
-  isLinux = lib.strings.hasSuffix "linux" host.platform;
+  isDarwin = lib.strings.hasSuffix "darwin" host.platform;
   darwinEnv =
+    # Rustup
+    ". \"$HOME/.cargo/env\"\n" +
+    # Home Manager
     "export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels\${NIX_PATH:+:$NIX_PATH}\n";
+  darwinInit =
+    # OPAM
+    "[[ ! -r $HOME/.opam/opam-init/init.zsh ]] || source $HOME/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null\n";
 in
 {
   programs = {
@@ -19,7 +25,9 @@ in
             "robbyrussell"
         );
       };
-      envExtra = lib.optionalString (!isLinux) darwinEnv;
+      envExtra = lib.optionalString isDarwin darwinEnv;
+      initExtra = lib.optionalString isDarwin darwinInit;
+      profileExtra = lib.optionalString isDarwin "\n";
     };
   };
 }
