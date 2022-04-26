f8b14745ff6a92fa0f4eb59b8bb517c1fb17ae92
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-04-26 17:59:16 +0200

    Update config

--- system/dev/postgresql.nix
+++ system/dev/postgresql.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, isDarwin, ... }:
+{ lib, pkgs, isDarwin, isLinux, ... }:
 
 {
   services = {
@@ -6,14 +6,15 @@
       enable = true;
       package = pkgs.postgresql_14;
       enableTCPIP = true;
+    } // lib.optionalAttrs isDarwin {
+      # https://github.com/LnL7/nix-darwin/issues/339
+      dataDir = "/usr/local/var/postgres";
+    } // lib.optionalAttrs isLinux {
       authentication = pkgs.lib.mkOverride 10 ''
         local all all trust
         host all all 127.0.0.1/32 trust
         host all all ::1/128 trust
       '';
-    } // lib.optionalAttrs isDarwin {
-      # https://github.com/LnL7/nix-darwin/issues/339
-      dataDir = "/usr/local/var/postgres";
     };
   };
 }
