d7cdd879252d326be1dbeda7189a577dba0e0c65
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-05-04 20:14:49 +0200

    Update config

--- system/www/apache.nix
+++ system/www/apache.nix
@@ -7,6 +7,12 @@
     enablePHP = true;
     virtualHosts."localhost" = {
       documentRoot = "/var/www/localhost";
+      extraConfig = ''
+        <Directory />
+          DirectoryIndex index.php
+          Require all granted
+        </Directory>
+      '';
     };
   };
 
