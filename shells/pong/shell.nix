0fcae42ce3dfc2a2fa063a5bb80f9cbd4afb506d
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-06-28 10:34:52 +0200

    Update pools

--- shells/pong/shell.nix
+++ shells/pong/shell.nix
@@ -9,6 +9,7 @@ pkgs.mkShell {
       pools = [
         "0xc08F53bd2787Bfb6057FDc60c3405E41274E8d8C"
         "0xDb7ade08134f7D4a992AdA5B7c07Cb3314dFD0c4"
+        "0x51dd1e0260A4a0157C4D242fE05cE0141B07B2b4"
       ];
     })
     perpetual-pools-keeper
