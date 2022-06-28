961e89c15e6199f2b925a217aabd3cbb39d866cd
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-06-28 15:44:17 +0200

    Update pools

--- shells/pong/shell.nix
+++ shells/pong/shell.nix
@@ -7,6 +7,7 @@ pkgs.mkShell {
     (perpetual-pools-client.override {
       pongified = true;
       pools = [
+        "0x2b6314479002ED5104F391989d4F761f71231b54"
         "0xaB1510e740F0c9247974e61d900eC964337d5D5A"
         "0x4Fd513f353Bc834aBd7Df5e054E35eC40812af30"
         "0xCb92788f7736f87f4a53fFcbb4B2AF70A65346ba"
