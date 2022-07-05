2d8a3e55a117339a9a1155a59db710aa984006ae
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-07-05 04:26:28 +0200

    Remove files on GitLab

--- shells/pocof3/device-xiaomi-alioth/weston-fixes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "sourcing /etc/X11/xinit/xinitrc.d/00-xiaomi-alioth-weston-fixes.sh" | logger -t "$(whoami):xiaomi-alioth-weston-fixes"
-export WESTON_DISABLE_ATOMIC=1
-export MESA_LOADER_DRIVER_OVERRIDE=msm
