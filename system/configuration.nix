d96d233d1fd0236c8c72bfaa9ce161d921047206
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-07-05 13:02:28 +0200

    Remove apps from mobile

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,16 +1,17 @@
 # System configuration.
 
-{ lib, isDarwin, isLinux, ... }:
+{ lib, isDarwin, isLinux, isMobile, ... }:
 
 {
   imports = [
     ./app/vscode.nix
     ./app/zsh.nix
-    ./dev/postgresql.nix
     ./media/font.nix
-    ./net/zerotier.nix
     ./sys/nix.nix
     ./x11/xorg-server.nix
+  ] ++ lib.optionals (!isMobile) [
+    ./dev/postgresql.nix
+    ./net/zerotier.nix
   ] ++ lib.optionals isDarwin [
     ./darwin.nix
   ] ++ lib.optionals isLinux [
--- users/weijia/home.nix
+++ users/weijia/home.nix
@@ -1,17 +1,18 @@
 # Home configuration.
 
-{ lib, isDarwin, isLinux, ... }:
+{ lib, isDarwin, isLinux, isMobile, ... }:
 
 {
   imports = [
-    ./app/alacritty.nix
-    ./app/vscodium.nix
     ./app/zsh.nix
     ./dev/android-tools.nix
     ./dev/git.nix
+    ./sys/nix.nix
+  ] ++ lib.optionals (!isMobile) [
+    ./app/alacritty.nix
+    ./app/vscodium.nix
     ./net/cloudflared.nix
     ./net/nodejs.nix
-    ./sys/nix.nix
   ] ++ lib.optionals isDarwin [
     ./darwin.nix
   ] ++ lib.optionals isLinux [
