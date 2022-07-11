493e063cd81de7863828e2e566b5aff25e312266
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-07-12 00:34:10 +0200

    Remove Mobile NixOS

--- hardware/pinephone/hardware-configuration.nix
+++ hardware/pinephone/hardware-configuration.nix
@@ -1,19 +1,45 @@
-{ lib, pkgs, nur-pkgs, mobile-nixos-src, ... }:
+{ lib, pkgs, nur-pkgs, ... }:
 
-let
-  kernelPackages = pkgs.linuxPackagesFor nur-pkgs.linux_pinephone;
-in
 {
-  imports = [
-    (import "${mobile-nixos-src}/lib/configuration.nix" {
-      device = "pine64-pinephone";
+  nixpkgs.overlays = [
+    (final: super: {
+      makeModulesClosure = x:
+        super.makeModulesClosure (x // { allowMissing = true; });
     })
   ];
 
-  boot.kernelPackages = lib.mkForce kernelPackages;
+  boot = {
+    kernelPackages = lib.mkForce
+      (pkgs.linuxPackagesFor nur-pkgs.linux_pinephone);
+    kernelParams = [
+      # Serial console on ttyS0, using the serial headphone adapter.
+      "console=ttyS0,115200"
+      "vt.global_cursor_default=0"
+      "earlycon=uart,mmio32,0x01c28000"
+      "panic=10"
+      "consoleblank=0"
+    ];
+    loader = {
+      efi.canTouchEfiVariables = true;
+      systemd-boot.enable = true;
+    };
+  };
 
-  mobile.boot.stage-1.kernel = {
-    package = lib.mkForce kernelPackages.kernel;
-    useNixOSKernel = true;
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+  };
+
+  hardware = {
+    enableRedistributableFirmware = true;
+    firmware = [
+      nur-pkgs.pinephone-firmware
+    ];
   };
 }
