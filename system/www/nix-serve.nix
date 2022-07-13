692cbd25188f13a8f0e9174771469c357307f721
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-07-13 08:00:47 +0200

    Configure reverse proxy

--- system/www/nix-serve.nix
+++ system/www/nix-serve.nix
@@ -1,6 +1,20 @@
+{ config, ... }:
+
 {
   services.nix-serve = {
     enable = true;
     secretKeyFile = "/var/cache-priv-key.pem";
+  };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."cache.weijia.wang" = {
+      locations."/".extraConfig = ''
+        proxy_pass http://localhost:${toString config.services.nix-serve.port};
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      '';
+    };
   };
 }
