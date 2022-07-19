1dcf86e712c6821dd419f654dd091884f81a0095
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-07-19 03:55:53 +0200

    Simulate armv7l

--- system/app/waydroid.nix
+++ system/app/waydroid.nix
@@ -1,8 +1,32 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
+let
+  qemu-user-arm = pkgs.qemu.override {
+    smartcardSupport = false;
+    spiceSupport = false;
+    openGLSupport = false;
+    virglSupport = false;
+    vncSupport = false;
+    gtkSupport = false;
+    sdlSupport = false;
+    pulseSupport = false;
+    smbdSupport = false;
+    seccompSupport = false;
+    hostCpuTargets = [ "arm-linux-user" ];
+  };
+in
 {
   virtualisation = {
     waydroid.enable = true;
     lxd.enable = true;
+  };
+
+  boot.binfmt = {
+    emulatedSystems = [
+      "armv7l-linux"
+    ];
+    registrations.armv7l-linux = {
+      interpreter = lib.mkForce "${qemu-user-arm}/bin/qemu-arm";
+    };
   };
 }
