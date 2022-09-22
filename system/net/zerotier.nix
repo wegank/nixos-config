be228b5b5877f8adaa4d26fbe5daca9f72a3c1fd
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-09-23 00:04:51 +0200

    zerotierone: hotfix

--- system/net/zerotier.nix
+++ system/net/zerotier.nix
@@ -1,4 +1,4 @@
-{ lib, isDarwin, isLinux, ... }:
+{ lib, pkgs, isDarwin, isLinux, ... }:
 
 { } // lib.optionalAttrs isDarwin {
   homebrew.casks = [
@@ -7,6 +7,17 @@
 } // lib.optionalAttrs isLinux {
   services.zerotierone = {
     enable = true;
+    package = with pkgs; zerotierone.overrideAttrs (old: {
+      cargoDeps = rustPlatform.importCargoLock {
+        lockFile = fetchurl {
+          url = "https://raw.githubusercontent.com/zerotier/ZeroTierOne/${old.version}/zeroidc/Cargo.lock";
+          sha256 = "sha256-pn7t7udZ8A72WC9svaIrmqXMBiU2meFIXv/GRDPYloc=";
+        };
+        outputHashes = {
+          "jwt-0.16.0" = "sha256-P5aJnNlcLe9sBtXZzfqHdRvxNfm6DPBcfcKOVeLZxcM=";
+        };
+      };
+    });
   };
 
   environment.unfreePackages = [
