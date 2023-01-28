7e12870db020b8b14e9feea3ef31c85c768bbdbd
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-01-28 21:13:40 +0100

    nix: trust nix-community cachix

--- system/sys/nix.nix
+++ system/sys/nix.nix
@@ -17,11 +17,13 @@
     settings = {
       auto-optimise-store = !isServer;
       substituters = [
+        "https://nix-community.cachix.org/"
         "https://wegank.cachix.org/"
       ] ++ lib.optionals isMobile [
         "https://cache.weijia.wang/"
       ];
       trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "wegank.cachix.org-1:xHignps7GtkPP/gYK5LvA/6UFyz98+sgaxBSy7qK0Vs="
         "cache.weijia.wang:2aRaA9TLlndTMKhIgKDdiHiy4JCEYR+N9011PU1VxNo="
       ];
