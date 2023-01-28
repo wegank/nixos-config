a22d1733c07b0537a0740717000cf9b22e2b66f1
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-01-28 10:52:26 +0100

    conda: farewell

--- users/weijia/app/zsh.nix
+++ users/weijia/app/zsh.nix
@@ -8,20 +8,6 @@ let
   '';
 
   darwinInit = ''
-    # >>> conda initialize >>>
-    # !! Contents within this block are managed by 'conda init' !!
-    __conda_setup="$('/Users/weijiawang/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-    if [ $? -eq 0 ]; then
-        eval "$__conda_setup"
-    else
-        if [ -f "/Users/weijiawang/miniconda3/etc/profile.d/conda.sh" ]; then
-            . "/Users/weijiawang/miniconda3/etc/profile.d/conda.sh"
-        else
-            export PATH="/Users/weijiawang/miniconda3/bin:$PATH"
-        fi
-    fi
-    unset __conda_setup
-    # <<< conda initialize <<<
   '';
 in
 {
