9d4bb29482d4c0689e1cd1b7e07502de333a2f75
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-02-13 13:56:52 +0100

    hyperv: initial support

---
+++ hardware/hyperv/hardware-configuration.nix
@@ -0,0 +1,40 @@
+# Hardware configuration.
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports = [ ];
+
+  boot = {
+    initrd = {
+      availableKernelModules = [
+        "sd_mod"
+        "sr_mod"
+      ];
+      kernelModules = [ ];
+    };
+    kernelModules = [ ];
+    extraModulePackages = [ ];
+    loader = {
+      systemd-boot.enable = true;
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot/efi";
+      };
+    };
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+    "/boot/efi" = {
+      device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+  };
+
+  swapDevices = [ ];
+
+  virtualisation.hypervGuest.enable = true;
+}
