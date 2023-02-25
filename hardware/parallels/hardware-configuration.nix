536dcd0b3e60d6fcc3cfd6e39fcb67e218db1076
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-02-25 11:33:44 +0100

    prl-tools: 18.1.1-53328 -> 18.2.0-53488

--- hardware/parallels/hardware-configuration.nix
+++ hardware/parallels/hardware-configuration.nix
@@ -38,10 +38,10 @@
   hardware.parallels = {
     enable = true;
     package = with pkgs; config.boot.kernelPackages.prl-tools.overrideAttrs (old: rec {
-      version = "18.1.1-53328";
+      version = "18.2.0-53488";
       src = fetchurl {
         url = "https://download.parallels.com/desktop/v${lib.versions.major version}/${version}/ParallelsDesktop-${version}.dmg";
-        hash = "sha256-Vw9i7Diki+hKODeosxfCY5bL/UOfwgzeCC6+QmWfIZw=";
+        hash = "sha256-FpAbQQapIcZ7GsGjH4ZeJ81Ke+NUF7GvgV1wEDLKoUU=";
       };
       patches = lib.optionals (lib.versionAtLeast config.boot.kernelPackages.kernel.version "6.0") [
         # ./prl-tools-6.0.patch
