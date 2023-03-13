9688c1040e72b559543b37c49e6c03f658e676e6
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-03-13 17:55:07 +0200

    kimsufi: update network config

--- hardware/kimsufi/hardware-configuration.nix
+++ hardware/kimsufi/hardware-configuration.nix
@@ -33,16 +33,27 @@
   networking = {
     useDHCP = false;
     usePredictableInterfaceNames = false;
-    interfaces."eth0".ipv4.addresses = [{
-      address = "5.39.78.26";
-      prefixLength = 24;
-    }];
+    interfaces."eth0" = {
+      ipv4.addresses = [{
+        address = "5.39.78.26";
+        prefixLength = 24;
+      }];
+      ipv6.addresses = [{
+        address = "2001:41d0:8:901a::1";
+        prefixLength = 128;
+      }];
+    };
     defaultGateway = {
       address = "5.39.78.254";
       interface = "eth0";
     };
+    defaultGateway6 = {
+      address = "2001:41d0:0008:90ff:00ff:00ff:00ff:00ff";
+      interface = "eth0";
+    };
     nameservers = [
       "8.8.8.8"
+      "8.8.4.4"
     ];
   };
 
