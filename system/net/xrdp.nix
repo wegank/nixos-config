04d4c15f8b5f060ae22543b332a48eda47d99c7c
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-03-13 17:10:30 +0200

    xrdp: harden

--- system/net/xrdp.nix
+++ system/net/xrdp.nix
@@ -1,14 +1,6 @@
 {
-  services = {
-    xrdp = {
-      enable = true;
-      defaultWindowManager = "xfce4-session";
-    };
-  };
-
-  networking = {
-    firewall = {
-      allowedTCPPorts = [ 3389 ];
-    };
+  services.xrdp = {
+    enable = true;
+    defaultWindowManager = "xfce4-session";
   };
 }
