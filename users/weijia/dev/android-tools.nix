82d39a5792e0f04562a9abd8eb19c0e7aa5e23ad
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-03-29 12:32:16 +0300

    android-tools: back to upstream on darwin

--- users/weijia/dev/android-tools.nix
+++ users/weijia/dev/android-tools.nix
@@ -1,25 +1,7 @@
-{ pkgs, config ? pkgs.config, isDarwin, isLinux, ... }:
+{ pkgs, ... }:
 
-let
-  androidEnvNixpkgs = fetchTarball {
-    name = "androidenv";
-    url = "https://github.com/NixOS/nixpkgs/archive/1fdeb86121ab6e5101e52b3fa2a6d8ebff740100.tar.gz";
-    sha256 = "sha256:0xg5lik0bl4ml6spyhwf644cd67gvs5xy5ffzh64dfsrzfapkjn7";
-  };
-
-  androidEnv = pkgs.callPackage "${androidEnvNixpkgs}/pkgs/development/mobile/androidenv" {
-    inherit config pkgs;
-    licenseAccepted = true;
-  };
-
-  androidComposition = androidEnv.composeAndroidPackages {
-    platformToolsVersion = "33.0.1";
-  };
-in
 {
-  home.packages = with pkgs; lib.optionals isDarwin [
-    androidComposition.platform-tools
-  ] ++ lib.optionals isLinux [
+  home.packages = with pkgs; [
     android-tools
   ];
 }
