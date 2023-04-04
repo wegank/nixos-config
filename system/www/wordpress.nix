0fe88655506abc21f67373883b6aa51aefc81ca1
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-04-04 11:13:54 +0300

    wordpress: update language

--- system/www/wordpress.nix
+++ system/www/wordpress.nix
@@ -2,20 +2,36 @@
 
 let
   domain = "in.con.nu";
+  version = "6.2";
 in
 {
   services.wordpress = {
     webserver = "nginx";
     sites.${domain} = {
-      package = with pkgs; wordpress.overrideAttrs (old: rec {
-        version = "6.2";
+      package = with pkgs; wordpress.overrideAttrs (old: {
+        inherit version;
         src = fetchurl {
           url = "https://wordpress.org/${old.pname}-${version}.tar.gz";
           hash = "sha256-FDEo3rZc7SU9yqAplUScSMUWOEVS0e/PsrOPjS9m+QQ=";
         };
       });
-      languages = [
-        pkgs.wordpressPackages.languages.fr_FR
+      languages = with pkgs; [
+        (stdenvNoCC.mkDerivation {
+          pname = "wordpress-language-fr";
+          inherit version;
+          src = fetchurl {
+            url = "https://fr.wordpress.org/wordpress-${version}-fr_FR.tar.gz";
+            hash = "sha256-H87YguPOQuuPH5T6G9AbJbCmAZmhAt2A21pNcbUnIgM=";
+          };
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out
+            cp -r ./wp-content/languages/* $out/
+
+            runHook postInstall
+          '';
+        })
       ];
       settings = {
         WPLANG = "fr_FR";
