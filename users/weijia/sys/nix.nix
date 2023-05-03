00e338b65b73ba18c6dff812ffd8a391cebad8f1
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-05-03 14:03:40 +0300

    nixpkgs-review: hotfix

--- users/weijia/sys/nix.nix
+++ users/weijia/sys/nix.nix
@@ -11,7 +11,15 @@
     packages = with pkgs; [
       cachix
       nixpkgs-fmt
-      nixpkgs-review
+      (nixpkgs-review.overrideAttrs (old: {
+        patches = [
+          (fetchpatch {
+            name = "fix-detection-of-broken-packages.patch";
+            url = "https://github.com/Mic92/nixpkgs-review/commit/1e67afb01e3a16bba617c3bb14752797c730a450.patch";
+            hash = "sha256-71pbcg+nFRUZZxpPKH93EFzyrAG3wVWMaCVIvgLaTH0=";
+          })
+        ];
+      }))
       rnix-lsp
     ];
   };
