7e72295d563faa3350ed1e4af6f5106007e3e931
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-05-25 12:16:05 +0300

    nixpkgs-review: upstreamed

--- users/weijia/sys/nix.nix
+++ users/weijia/sys/nix.nix
@@ -12,15 +12,7 @@
       cachix
       nil
       nixpkgs-fmt
-      (nixpkgs-review.overrideAttrs (old: {
-        patches = [
-          (fetchpatch {
-            name = "fix-detection-of-broken-packages.patch";
-            url = "https://github.com/Mic92/nixpkgs-review/commit/1e67afb01e3a16bba617c3bb14752797c730a450.patch";
-            hash = "sha256-71pbcg+nFRUZZxpPKH93EFzyrAG3wVWMaCVIvgLaTH0=";
-          })
-        ];
-      }))
+      nixpkgs-review
     ];
   };
 
