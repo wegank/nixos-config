43a7332f0651b8614872d5fa89947cf9301dd66d
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-05-25 13:07:24 +0300

    fish: update shellInit

--- users/weijia/app/fish.nix
+++ users/weijia/app/fish.nix
@@ -1,17 +1,14 @@
 { lib, isDarwin, ... }:
 
-let
-  darwinInit = ''
-    fish_add_path --path --move $HOME/.local/bin $HOME/.nix-profile/bin /run/wrappers/bin /nix/var/nix/profiles/default/bin /run/current-system/sw/bin
-    fish_add_path --path --move $HOME/.npm-global/bin
-    fish_add_path --path --move $HOME/.cargo/bin
-  '';
-in
 {
-  programs = {
-    fish = {
-      enable = true;
-      shellInit = lib.optionalString isDarwin darwinInit;
-    };
+  programs.fish = {
+    enable = true;
+    shellInit = ''
+      set fish_greeting
+    '' + lib.optionalString isDarwin ''
+      fish_add_path --path --move $HOME/.local/bin $HOME/.nix-profile/bin /run/wrappers/bin /nix/var/nix/profiles/default/bin /run/current-system/sw/bin
+      fish_add_path --path --move $HOME/.npm-global/bin
+      fish_add_path --path --move $HOME/.cargo/bin
+    '';
   };
 }
