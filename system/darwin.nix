bd08ee757618786f9230576829b95a7674d64699
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-06-07 12:40:49 +0300

    darwin: set home

--- system/darwin.nix
+++ system/darwin.nix
@@ -1,7 +1,13 @@
+{ owner, ... }:
+
 {
   imports = [
     ./sys/brew.nix
   ];
+
+  users.users.${owner.name} = {
+    home = "/Users/${owner.name}";
+  };
 
   # Set state version.
   system.stateVersion = 4;
