b9d8f9dd302981c7629bc5b9b676d7998e11c358
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-06-16 15:26:09 +0300

    virt-manager: upstreamed fixes

--- system/app/virt-manager.nix
+++ system/app/virt-manager.nix
@@ -1,17 +1,8 @@
-{ lib, pkgs, owner, isLinux, isDarwin, ... }:
+{ lib, pkgs, owner, isLinux, ... }:
 
 {
   environment.systemPackages = with pkgs; [
-    ((virt-manager.override {
-      xorriso = pkgs.xorriso.overrideAttrs (old: {
-        env.NIX_CFLAGS_COMPILE = lib.optionalString isDarwin "-include unistd.h";
-      });
-    }).overrideAttrs (old: {
-      disabledTestPaths = (old.disabledTestPaths or [ ]) ++ lib.optionals isDarwin [
-        "tests/test_checkprops.py"
-        "tests/test_cli.py"
-      ];
-    }))
+    virt-manager
   ];
 } // lib.optionalAttrs isLinux {
   virtualisation.libvirtd.enable = true;
