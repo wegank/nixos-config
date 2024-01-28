e18531a8ccbe59427ab77a6d0d22612419652260
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-01-29 00:38:40 +0100

    hardware: enable systemd-boot on rpi

--- hardware/raspberrypi/hardware-configuration.nix
+++ hardware/raspberrypi/hardware-configuration.nix
@@ -19,15 +19,17 @@
     kernelModules = [ ];
     extraModulePackages = [ ];
     loader = {
-      grub.enable = false;
-      generic-extlinux-compatible.enable = true;
+      systemd-boot.enable = true;
+      efi = {
+        canTouchEfiVariables = true;
+      };
     };
   };
 
   fileSystems = {
     "/" = {
       device = "/dev/disk/by-label/nixos";
-      fsType = "ext4";
+      fsType = "btrfs";
     };
     "/boot" = {
       device = "/dev/disk/by-label/boot";
@@ -38,29 +40,6 @@
   swapDevices = [ ];
 
   powerManagement.cpuFreqGovernor = "ondemand";
-
-  system.activationScripts.firmware-update =
-    let configTxt = pkgs.writeText "config.txt" ''
-      [pi4]
-      kernel=u-boot-rpi4.bin
-      enable_gic=1
-      armstub=armstub8-gic.bin
-      disable_overscan=1
-      [all]
-      arm_64bit=1
-      enable_uart=1
-      avoid_warnings=1
-    '';
-    in
-    ''
-      (cd ${pkgs.raspberrypifw}/share/raspberrypi/boot && cp bootcode.bin fixup*.dat start*.elf /boot/)
-      # Add the config
-      cp ${configTxt} /boot/config.txt
-      # Add pi4 specific files
-      cp ${pkgs.ubootRaspberryPi4_64bit}/u-boot.bin /boot/u-boot-rpi4.bin
-      cp ${pkgs.raspberrypi-armstubs}/armstub8-gic.bin /boot/armstub8-gic.bin
-      cp ${pkgs.raspberrypifw}/share/raspberrypi/boot/bcm2711-rpi-4-b.dtb /boot/
-    '';
 
   hardware.enableRedistributableFirmware = true;
 }
