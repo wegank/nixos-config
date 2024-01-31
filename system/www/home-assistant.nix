25d12177a01554aa0b8d7a2c4fb8e27bcd1ded94
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-01-31 12:51:04 +0100

    home-assistant: drop podman

--- system/www/home-assistant.nix
+++ system/www/home-assistant.nix
@@ -1,17 +1,14 @@
 { ... }:
 
 {
-  virtualisation.oci-containers = {
-    backend = "podman";
-    containers.homeassistant = {
-      volumes = [
-        "home-assistant:/config"
-      ];
-      environment.TZ = "Europe/Paris";
-      image = "ghcr.io/home-assistant/home-assistant:stable";
-      extraOptions = [
-        "--network=host"
-      ];
+  nixpkgs.config.permittedInsecurePackages = [
+    "openssl-1.1.1w"
+  ];
+
+  services.home-assistant = {
+    enable = true;
+    config = {
+      default_config = { };
     };
   };
 }
