153ef32182552305f34328d565153057ebc8d9e0
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-01-31 11:45:54 +0100

    hardware: enable bluetooth on rpi

--- hardware/raspberrypi/hardware-configuration.nix
+++ hardware/raspberrypi/hardware-configuration.nix
@@ -41,5 +41,18 @@
 
   powerManagement.cpuFreqGovernor = "ondemand";
 
+  # Wi-Fi
   hardware.enableRedistributableFirmware = true;
+
+  # Bluetooth
+  hardware.bluetooth.enable = true;
+
+  systemd.services.btattach = {
+    before = [ "bluetooth.service" ];
+    after = [ "dev-ttyAMA1.device" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.bluez}/bin/btattach -B /dev/ttyAMA1 -P bcm -S 3000000";
+    };
+  };
 }
