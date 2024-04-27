a162e14b9d7e87d73610d378e51c2887c3d49ad6
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-04-27 06:57:32 +0200

    vscode-server: drop patch

--- system/app/vscode-server.nix
+++ system/app/vscode-server.nix
@@ -1,18 +1,7 @@
-{ pkgs, ... }:
-
-let
-  vscode_1_82 = pkgs.fetchpatch {
-    url = "https://github.com/nix-community/nixos-vscode-server/pull/68.patch";
-    hash = "sha256-kZWOcw3QKZK598nWj9UuMCJlQxQ/qzBKqACkQO8+jvI=";
-  };
-in
 {
   services = {
     vscode-server = {
       enable = true;
-      postPatch = ''
-        patch -p1 < ${vscode_1_82}
-      '';
     };
   };
 }
