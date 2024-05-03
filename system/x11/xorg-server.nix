a36a68ebcb981104b4c7d229fa144a840bef5e75
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-05-03 11:32:12 +0200

    xserver: move libinput to services

--- system/x11/xorg-server.nix
+++ system/x11/xorg-server.nix
@@ -1,15 +1,18 @@
 { lib, pkgs, isDarwin, isLinux, ... }:
 
-{ } // lib.optionalAttrs isDarwin {
+lib.optionalAttrs isDarwin {
   homebrew.casks = [
     "xquartz"
   ];
 } // lib.optionalAttrs isLinux {
   services.xserver = {
     enable = true;
-    libinput.enable = true;
     excludePackages = with pkgs; [
       xterm
     ];
+  };
+
+  services.libinput = {
+    enable = true;
   };
 }
