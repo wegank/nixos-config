053ccb024f97368fbaf38d45c759198ec45a75c2
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-06-24 11:57:36 +0200

    fish: add path to julia on darwin

--- users/weijia/app/fish.nix
+++ users/weijia/app/fish.nix
@@ -9,6 +9,7 @@
       fish_add_path --path --move /run/current-system/sw/bin
       fish_add_path $HOME/.npm-global/bin
       fish_add_path $HOME/.cargo/bin
+      fish_add_path $HOME/.juliaup/bin
     '';
   };
 }
