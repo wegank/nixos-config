d8af78894890e798a0be745c25f24b4f05cd9c34
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-07-16 23:38:13 +0200

    gtk: move theme to top-level

--- users/weijia/x11/gtk.nix
+++ users/weijia/x11/gtk.nix
@@ -9,7 +9,7 @@
     };
     iconTheme = {
       name = "Adwaita";
-      package = pkgs.gnome.adwaita-icon-theme;
+      package = pkgs.adwaita-icon-theme;
     };
     gtk3 = {
       extraConfig = {
