2d87f5b406f608f3e4813e0482a7f0b8588c763e
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-09-10 17:13:49 +0200

    darwin: update nixbld uid

--- system/darwin.nix
+++ system/darwin.nix
@@ -1,4 +1,4 @@
-{ owner, ... }:
+{ owner, lib, ... }:
 
 {
   imports = [ ./sys/brew.nix ];
@@ -6,6 +6,8 @@
   users.users.${owner.name} = {
     home = "/Users/${owner.name}";
   };
+
+  ids.uids.nixbld = lib.mkForce 350;
 
   # Set state version.
   system.stateVersion = 4;
