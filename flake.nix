7ec95949ad0c30db23c448738dcfddf094146203
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-12-09 14:12:55 +0100

    flake: switch to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable-small";
+      url = "github:nixos/nixpkgs/nixos-unstable";
     };
     home-manager = {
       url = "github:nix-community/home-manager";
