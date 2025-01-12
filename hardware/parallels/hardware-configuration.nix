73aa66d205e0392e3d7896024dace66fff99ce24
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-01-12 07:49:31 +0100

    linuxPackages_latest.prl-tools: 20.1.3-55743 -> 20.2.0-55872

--- hardware/parallels/hardware-configuration.nix
+++ hardware/parallels/hardware-configuration.nix
@@ -46,13 +46,23 @@
     enable = true;
     package = config.boot.kernelPackages.prl-tools.overrideAttrs (
       finalAttrs: previousAttrs: {
-        version = "20.1.3-55743";
+        version = "20.2.0-55872";
         src = previousAttrs.src.overrideAttrs {
-          outputHash = "sha256-5lbTTQucop/jnsVudoqTO9bESR5tdn8NFu9Nm2WphU4=";
+          outputHash = "sha256-oOilbF5MzZxZXNVQYAp/JxyMVdM0oltG8pGfzzsQ1kY=";
         };
-        patches = [
-          ./linux-6.12.patch
-        ];
+        installPhase =
+          builtins.replaceStrings
+            [
+              "cp prl_fs/SharedFolders/Guest/Linux/prl_fs/prl_fs.ko $out/lib/modules/${config.boot.kernelPackages.kernel.modDirVersion}/extra"
+              "mkdir -p $out/share/man/man8"
+              "install -Dm644 ../mount.prl_fs.8 $out/share/man/man8"
+            ]
+            [
+              ""
+              ""
+              ""
+            ]
+            previousAttrs.installPhase;
       }
     );
   };
