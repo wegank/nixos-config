3535bed52830d4940404d5c5fa427f34c67acecd
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-01-22 10:40:12 +0100

    cups: configure wireless printers

--- system/net/cups.nix
+++ system/net/cups.nix
@@ -1,7 +1,17 @@
 {
   services = {
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+    };
     printing = {
       enable = true;
+      browsing = true;
+      browsedConf = ''
+        BrowseRemoteProtocols dnssd cups
+        LocalQueueNamingRemoteCUPS RemoteName
+        CreateIPPPrinterQueues All
+      '';
     };
   };
 }
