3ecc46d8cab2be3343657b22f076df0c27d4bf56
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-03-24 01:24:14 +0100

    nextcloud: 30 -> 31

--- system/www/nextcloud.nix
+++ system/www/nextcloud.nix
@@ -12,7 +12,7 @@ in
 {
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud30;
+    package = pkgs.nextcloud31;
     config = {
       adminuser = owner.name;
       adminpassFile = "${pkgs.writeText "adminPass" owner.initialPassword}";
