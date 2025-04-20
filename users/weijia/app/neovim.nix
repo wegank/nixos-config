40159cdc166537ecaf7ca76aade73b8b2260def6
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-04-21 00:02:54 +0200

    neovim: fix coc on darwin

--- users/weijia/app/neovim.nix
+++ users/weijia/app/neovim.nix
@@ -8,12 +8,12 @@
       package =
         with pkgs;
         vimPlugins.coc-nvim.overrideAttrs {
-          version = "2025-04-15";
+          version = "2025-04-06";
           src = fetchFromGitHub {
             owner = "neoclide";
             repo = "coc.nvim";
-            rev = "55fda716adda510e638fb4588ccbc93249c2d8d1";
-            hash = "sha256-DSzCpFsKsIOHaxLI1ub9lYelrOy9qm3qhtL37/ql5lA=";
+            rev = "341a73d9a4195f2a4c8056b5dc668564cc9914f5";
+            hash = "sha256-SP3qQXgeIE90nY1RVvhWxv0CExVLy+8g1ZCBLEo8uOA=";
           };
         };
     };
