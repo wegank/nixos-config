234a098a6eb50b7f72ef9ee59d6179f5fc5c09be
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-05-02 11:51:01 +0200

    neovim: drop coc overrides

--- users/weijia/app/neovim.nix
+++ users/weijia/app/neovim.nix
@@ -1,24 +1,8 @@
-{ pkgs, ... }:
-
 {
   programs.neovim = {
     enable = true;
     coc = {
       enable = true;
-      package =
-        with pkgs;
-        vimPlugins.coc-nvim.overrideAttrs {
-          version = "2025-04-06";
-          src = fetchFromGitHub {
-            owner = "neoclide";
-            repo = "coc.nvim";
-            rev = "341a73d9a4195f2a4c8056b5dc668564cc9914f5";
-            postFetch = ''
-              rm -f "$out/README.md" "$out/Readme.md"
-            '';
-            hash = "sha256-RvUUYvPn7CLpSEPBgI6BhMXnA32jA6GCQQG/VYGNTu8=";
-          };
-        };
     };
     defaultEditor = true;
   };
