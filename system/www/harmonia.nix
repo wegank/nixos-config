88cc445f73ea048ea82781906fee620c20eb764e
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-08-21 21:19:26 +0200

    harmonia: init

---
+++ system/www/harmonia.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  pkgs,
+  owner,
+  ...
+}:
+
+{
+  services.harmonia.enable = true;
+  services.harmonia.signKeyPaths = [ "/var/lib/secrets/harmonia.secret" ];
+
+  nix.settings.allowed-users = [ "harmonia" ];
+
+  networking.firewall.allowedTCPPorts = [
+    443
+    80
+  ];
+
+  security.acme.defaults.email = owner.email;
+  security.acme.acceptTerms = true;
+
+  services.nginx = {
+    enable = true;
+    recommendedTlsSettings = true;
+    virtualHosts."me.con.nu" = {
+      enableACME = true;
+      forceSSL = true;
+
+      locations."/".extraConfig = ''
+        proxy_pass http://127.0.0.1:5000;
+        proxy_set_header Host $host;
+        proxy_redirect http:// https://;
+        proxy_http_version 1.1;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection $connection_upgrade;
+      '';
+    };
+  };
+}
