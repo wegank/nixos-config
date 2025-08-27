b0f5d56fbefa2c563e86092e3dc02c88b6422273
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-08-27 20:29:24 +0200

    openssh: add new server

--- users/weijia/net/openssh.nix
+++ users/weijia/net/openssh.nix
@@ -2,6 +2,11 @@
   programs.ssh = {
     enable = true;
     matchBlocks = {
+      "nanmen" = {
+        hostname = "120.202.241.46";
+        port = 32203;
+        user = "wegank";
+      };
       "ssh.lip6.fr" = {
         user = "weijia";
       };
