999e483458ffcc4e82bec80052538bdc21029a78
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-08-29 23:51:11 +0200

    openssh: disable default config

--- users/weijia/net/openssh.nix
+++ users/weijia/net/openssh.nix
@@ -1,7 +1,20 @@
 {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
     matchBlocks = {
+      "*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
       "nanmen" = {
         hostname = "120.202.241.46";
         port = 32203;
