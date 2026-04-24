68253acbea9d1d90cf67479e6444c1fdb6130a34
Weijia Wang <9713184+wegank@users.noreply.github.com>
2026-04-25 00:10:41 +0200

    emacs: drop obsolete override on darwin

--- users/weijia/app/emacs.nix
+++ users/weijia/app/emacs.nix
@@ -8,13 +8,7 @@
 {
   programs.emacs = {
     enable = true;
-    package =
-      if isDarwin then
-        pkgs.emacs30.override {
-          withMailutils = false;
-        }
-      else
-        pkgs.emacs30;
+    package = pkgs.emacs30;
     extraConfig = lib.optionalString isDarwin ''
       (setq mac-right-option-modifier 'nil)
     '';
