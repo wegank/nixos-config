15af9359d71ecee84d1163d83cbfb93d734d1eda
Weijia Wang <9713184+wegank@users.noreply.github.com>
2026-08-21 10:50:56 +0200

    nix: drop inaccessible build machines

--- users/weijia/sys/nix.nix
+++ users/weijia/sys/nix.nix
@@ -5,18 +5,7 @@
 }:
 
 {
-  nix.buildMachines = [
-    ({
-      hostName = "120.202.241.46:32203";
-      sshKey = "${config.home.homeDirectory}/.ssh/id_ed25519";
-      sshUser = "wegank";
-      supportedFeatures = [
-        "big-parallel"
-        "gccarch-la64v1.0"
-      ];
-      systems = [ "loongarch64-linux" ];
-    })
-  ];
+  nix.buildMachines = [ ];
 
   nix.distributedBuilds = true;
 
